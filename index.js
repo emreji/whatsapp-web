fd5ea49870ace94536edcb71c5adb17c94147be8
Elizabeth <lizareji23@gmail.com>
2018-03-09 20:54:26 -0500

    Initial commit of socket.io messaging

--- index.js
+++ index.js
@@ -6,14 +6,36 @@ let server = http.createServer(app);
 let io = require('socket.io')(server);
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/node_modules'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
- })
+ });
 
 io.on('connection', function(socket) {
     console.log('socket connected!');
+
+    socket.on('disconnect', function() {
+        console.log('User disconnected.');
+    });
+
+    socket.on('msg', function(data) {
+        io.sockets.emit('newmsg', data);
+    });
+});
+
+var clients = 0;
+io.on('connection', function(socket) {
+    clients++;
+    io.sockets.emit('broadcast', {description: clients + 'clients connected!'});
+    socket.on('disconnect', function () {
+        clients--;
+        io.sockets.emit('broadcast', {description: clients + 'clients connected!'});
+    })
 })
+
+
 
 server.listen(3000, function() {
     console.log('server running!', __dirname);
-})
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,34 @@
+var socket = io();
+
 function sendMessage() {
 	var message = $('#new-message').val();
 	if (message != "" && message != null) {
-		var currentDate = new Date();
-		var timeIndicator = (currentDate.getHours() < 12) ? "AM" : "PM";
-		var displayTime = currentDate.getHours() % 12 + ":" + currentDate.getMinutes() + " " + timeIndicator;
+
+		socket.emit('msg', {message: message});
 		
-		var chatBubbleDiv = document.createElement('div');
-		var messageSpan = document.createElement('span');
-		var timeDiv = document.createElement('div');
-		timeDiv.innerHTML = displayTime;
-
-		chatBubbleDiv.appendChild(messageSpan);
-		messageSpan.innerHTML = message;
-		messageSpan.appendChild(timeDiv);
-
-		chatBubbleDiv.setAttribute("class", "chat-bubble chat-bubble-sent");
-		$('.chat-window').append(chatBubbleDiv);
-		$('#new-message').val("");
 
 		scrollToBottomOfChatWindow()
 	}
 }
+
+socket.on('newmsg', function (data) {
+	var currentDate = new Date();
+	var timeIndicator = (currentDate.getHours() < 12) ? "AM" : "PM";
+	var displayTime = currentDate.getHours() % 12 + ":" + currentDate.getMinutes() + " " + timeIndicator;
+	
+	var chatBubbleDiv = document.createElement('div');
+	var messageSpan = document.createElement('span');
+	var timeDiv = document.createElement('div');
+	timeDiv.innerHTML = displayTime;
+
+	chatBubbleDiv.appendChild(messageSpan);
+	messageSpan.innerHTML = data.message;
+	messageSpan.appendChild(timeDiv);
+
+	chatBubbleDiv.setAttribute("class", "chat-bubble chat-bubble-sent");
+	$('.chat-window').append(chatBubbleDiv);
+	$('#new-message').val("");
+})
 
 function scrollToBottomOfChatWindow() {
 	var chatWindow = document.getElementById("chat-window");
